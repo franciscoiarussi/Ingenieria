4c6a5027ea919100d4ceb3053b5d070b38be6346
franciscoiarussi <franciscoiarussi74@gmail.com>
2020-05-29 16:22:28 +0200

    Prender y apagar Bluetooth

--- app/src/main/java/com/example/ingenieria/MainActivity.java
+++ app/src/main/java/com/example/ingenieria/MainActivity.java
@@ -2,13 +2,89 @@ package com.example.ingenieria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    BluetoothAdapter nBluetoothAdapter;
+
+    //Crear BroadcasteReceiver
+    private final BroadcastReceiver nBroadcastReceiver1 = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(nBluetoothAdapter.ACTION_STATE_CHANGED)){
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, nBluetoothAdapter.ERROR);
+
+                switch(state){
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG,"nBroadcastReceiver1: APAGADO");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d(TAG,"nBroadcastReceiver1: APAGANDOSE");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG,"nBroadcastReceiver1: PRENDIDO");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d(TAG,"nBroadcastReceiver1: PRENDIENDO");
+                        break;
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy:called");
+        super.onDestroy();
+        unregisterReceiver(nBroadcastReceiver1);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnONOFF = (Button) findViewById(R.id.btnONOFF);
+
+        nBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        btnONOFF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onCLick: prendidendo/apagando");
+                enableDisableBT();
+            }
+        });
+    }
+
+    public void enableDisableBT(){
+        if(nBluetoothAdapter == null){
+            Log.d(TAG, "enbleDisableBT: No tenes un adaptador Bluetooth");
+        }
+        if(!nBluetoothAdapter.isEnabled()){
+            Log.d(TAG, "enableDisableBT: prendidendo");
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBTIntent);
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(nBroadcastReceiver1, BTIntent);
+        }
+        if(nBluetoothAdapter.isEnabled()){
+            Log.d(TAG, "enableDisableBT: apagando");
+            nBluetoothAdapter.disable();
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(nBroadcastReceiver1, BTIntent);
+        }
+
     }
 }
